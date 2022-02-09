1d6229f7d4ea5980ecca9f9bf2585c3d5c3c9a17
im-kamil <kamilnasar786@gmail.com>
2022-02-09 13:05:10 +0530

    characterInput.java

---
+++ CharacterInput.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class CharacterInput {
+    public static void main(String[] args) {
+        // print character in java
+        // like input 0---->0
+        // same character input A---->A lets solve
+        char ch;
+        System.out.println("Please Enter Character ");
+        Scanner s = new Scanner(System.in);
+        ch = s.next().charAt(0);
+        System.out.println(ch);
+    }
+}
--- OddEven.java
+++
@@ -1,5 +0,0 @@
-public class OddEven {
-   public static void main(String[] args) {
-       System.out.println("Kamil kammo ofdd even");
-   } 
-}
