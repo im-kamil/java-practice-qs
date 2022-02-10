6cccfe711ef0609240a589b7c23fbf4a796b59c4
im-kamil <kamilnasar786@gmail.com>
2022-02-10 10:23:38 +0530

    AtoZalphabet.java

---
+++ AtoZalphabet.java
@@ -0,0 +1,8 @@
+public class AtoZalphabet {
+    public static void main(String[] args) {
+        // print AtoZ aphabet
+        for (char i = 'A'; i <= 'Z'; i++) {
+            System.out.print(i + " ");
+        }
+    }
+}
