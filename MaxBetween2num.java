3a5521af923fe73e3261181b67b4b1565f1acec6
im-kamil <kamilnasar786@gmail.com>
2022-02-10 10:29:04 +0530

    MaxBetween2num.java

---
+++ MaxBetween2num.java
@@ -0,0 +1,12 @@
+public class MaxBetween2num {
+    public static void main(String[] args) {
+        // maximum between two numbers
+        // input 10,20 -----> 20
+        int a = 10, b = 20;
+        if (a > b) {
+            System.out.print(a);
+        } else {
+            System.out.print(b);
+        }
+    }
+}
