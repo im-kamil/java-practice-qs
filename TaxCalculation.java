e0abbe5de3c86713938dd7a7a19a46f41b91553d
im-kamil <kamilnasar786@gmail.com>
2022-02-11 11:53:44 +0530

    Tax Calculation.java

--- TaxCalculation.java
+++ TaxCalculation.java
@@ -1,3 +1,24 @@
+import java.util.Scanner;
+
 public class TaxCalculation {
-    
+    public static void main(String[] args) {
+        // print tax calculation program
+        // input salary 10000----> no tax
+        // input salary >=10000 between <=100000 ----> 10%tax
+        // input salary 2000000-----> 20%tax
+        int salary;
+        double tax;
+        System.out.print("Enter your salary ");
+        Scanner s = new Scanner(System.in);
+        salary = s.nextInt();
+        if (salary <= 10000) {
+            System.out.print(salary + " no tax ");
+        } else if (salary > 10000 && salary <= 100000) {
+            tax = salary * 0.10;
+            System.out.print(salary + " " + tax);
+        } else {
+            tax = salary * 0.20;
+            System.out.print(salary + " " + tax);
+        }
+    }
 }
