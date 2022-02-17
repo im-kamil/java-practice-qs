3f014faa495d0bfaf8c5d5503e4daf1f168d9cf2
im-kamil <kamilnasar786@gmail.com>
2022-02-17 11:32:32 +0530

    Palindrome number .java

---
+++ palindromenumprogram.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class palindromenumprogram {
+    public static void main(String[] args) {
+        // palindrome number program
+        // input number 121 ---> 121 output
+        int n, r, s = 0, c;
+        System.out.print("Enter palindrome number ");
+        Scanner sc = new Scanner(System.in);
+        n = sc.nextInt();
+        c = n;
+        while (n > 0) {
+            r = n % 10;
+            s = (s * 10) + r; // s=0 because we r adding and 1 because we r multiplying
+            n = n / 10;
+        }
+        if (s == n) {
+            System.out.println("Palindrome number ");
+        } else {
+            System.out.println(" Not Palindrome number ");
+
+        }
+    }
+}
