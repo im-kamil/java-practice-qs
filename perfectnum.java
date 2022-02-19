143fe96f02edecb76c379da3287b9fb2b9e91b5c
im-kamil <kamilnasar786@gmail.com>
2022-02-19 12:20:37 +0530

    perfect number

--- perfectnum.java
+++ perfectnum.java
@@ -1,5 +1,22 @@
+import java.util.Scanner;
+
 public class perfectnum {
     public static void main(String[] args) {
-        // perfect number 
+        // perfect number input num 6---->1,2,3,4,5,6
+        // 6 ---->1+2+3=6 is perfect number
+        int n, sum = 0;
+        System.out.print("Enter any number ");
+        Scanner s = new Scanner(System.in);
+        n = s.nextInt();
+        for (int i = 1; i < n; i++) {
+            if (n % i == 0) {
+                sum = sum + i;
+            }
+        }
+        if (n == sum) {
+            System.out.println("Perfect number ");
+        } else {
+            System.out.println("Not perfect number ");
+        }
     }
 }
