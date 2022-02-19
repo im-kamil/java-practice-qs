723e209e4d754e527639faf2e1f4fbe9f168a825
im-kamil <kamilnasar786@gmail.com>
2022-02-19 12:53:46 +0530

    print Array element

---
+++ printArrayElements.java
@@ -0,0 +1,12 @@
+public class printArrayElements {
+    public static void main(String[] args) {
+        // different ways to print array elements
+        int a[] = new int[3];
+        a[0] = 10;
+        a[1] = 20;
+        a[2] = 30;
+        for (int i = 0; i < 3; i++) {
+            System.out.println(a[i] + " ");
+        }
+    }
+}
