231aa7d46de32084b8a9b91709458a9610b0cb87
im-kamil <kamilnasar786@gmail.com>
2022-02-22 12:52:39 +0530

    Array class methods equals()

--- ArrayClassMethod.java
+++ ArrayClassMethod.java
@@ -4,15 +4,31 @@ import java.util.Scanner;
 public class ArrayClassMethod {
     public static void main(String[] args) {
         // sort() equals() copyOf()
+        /*
+         * int a[] = new int[5];
+         * System.out.print("Enter data in Array ");
+         * Scanner s = new Scanner(System.in);
+         * for (int i = 0; i < a.length; i++) {
+         * a[i] = s.nextInt();
+         * }
+         * Arrays.sort(a);
+         * for (int i = 0; i < a.length; i++) {
+         * System.out.println(a[i] + " ");
+         * }
+         */
+        // equals()
         int a[] = new int[5];
-        System.out.print("Enter data in Array ");
+        int b[] = new int[5];
+        System.out.print("Enter data in Array 1 ");
         Scanner s = new Scanner(System.in);
         for (int i = 0; i < a.length; i++) {
             a[i] = s.nextInt();
         }
-        Arrays.sort(a);
-        for (int i = 0; i < a.length; i++) {
-            System.out.println(a[i] + " ");
+        System.out.print("Enter data in Array 2 ");
+        for (int i = 0; i < b.length; i++) {
+            b[i] = s.nextInt();
         }
+        Arrays.equals(a, b);
+        System.out.print("Kya dono equals hai " + b);
     }
 }
