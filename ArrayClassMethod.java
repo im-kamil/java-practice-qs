81a1a4af1665271c4435fa593a0d7d9a866f6b10
im-kamil <kamilnasar786@gmail.com>
2022-02-22 13:00:37 +0530

    ArrayClassMethod copyOf()

--- ArrayClassMethod.java
+++ ArrayClassMethod.java
@@ -17,18 +17,34 @@ public class ArrayClassMethod {
          * }
          */
         // equals()
+        /*
+         * int a[] = new int[5];
+         * int b[] = new int[5];
+         * System.out.print("Enter data in Array 1 ");
+         * Scanner s = new Scanner(System.in);
+         * for (int i = 0; i < a.length; i++) {
+         * a[i] = s.nextInt();
+         * }
+         * System.out.print("Enter data in Array 2 ");
+         * for (int i = 0; i < b.length; i++) {
+         * b[i] = s.nextInt();
+         * }
+         * Arrays.equals(a, b);
+         * System.out.print("Kya dono equals hai " + b);
+         */
+        // copyOf()
         int a[] = new int[5];
-        int b[] = new int[5];
-        System.out.print("Enter data in Array 1 ");
+        System.out.print("Enter data in Array ");
         Scanner s = new Scanner(System.in);
         for (int i = 0; i < a.length; i++) {
             a[i] = s.nextInt();
         }
-        System.out.print("Enter data in Array 2 ");
-        for (int i = 0; i < b.length; i++) {
-            b[i] = s.nextInt();
+
+        int a2[] = Arrays.copyOf(a, 5);
+        System.out.print("Data in Array 2 ");
+        for (int i = 0; i < a.length; i++) {
+            System.out.print(a2[i] + " ");
         }
-        Arrays.equals(a, b);
-        System.out.print("Kya dono equals hai " + b);
+
     }
 }
