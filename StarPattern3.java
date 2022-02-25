d6762cfe028ea2ef0ed92aaebbf1ce2f0b933460
im-kamil <kamilnasar786@gmail.com>
2022-02-25 12:36:41 +0530

    TriangleStarPttern in java

---
+++ StarPattern3.java
@@ -0,0 +1,15 @@
+public class StarPattern3 {
+    public static void main(String[] args) {
+        // star pattern 3
+        int i, j, k;
+        for (i = 1; i <= 5; i++) {
+            for (j = 5; j > i; j--) {
+                System.out.print(" ");
+            }
+            for (k = 1; k <= i; k++) {
+                System.out.print("* ");
+            }
+            System.out.print("\n");
+        }
+    }
+}
