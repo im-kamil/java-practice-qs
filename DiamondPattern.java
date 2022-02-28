9394ba0959ca900ea1bf05ca91a793fb1e36b891
im-kamil <kamilnasar786@gmail.com>
2022-02-28 11:33:16 +0530

    Diamond short star trick pattern

---
+++ DiamondPattern.java
@@ -0,0 +1,17 @@
+public class DiamondPattern {
+    public static void main(String[] args) {
+        // diamond pattern
+        int i, j, k;
+        for (i = 1; i <= 5; i++) { // rows
+            for (j = 5; j > i; j--) { // space(column)
+                System.out.print(" ");
+            }
+                for (k = 1; k <= (2 * i - 1); k++) {
+                    System.out.print("*");
+                }
+                System.out.println();
+            }
+
+        }
+    }
+
