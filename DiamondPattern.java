8d4147f720b77789d960249e70b2aa8b836b909b
im-kamil <kamilnasar786@gmail.com>
2022-02-28 11:46:27 +0530

    Diamond pattern

--- DiamondPattern.java
+++ DiamondPattern.java
@@ -6,12 +6,20 @@ public class DiamondPattern {
             for (j = 5; j > i; j--) { // space(column)
                 System.out.print(" ");
             }
-                for (k = 1; k <= (2 * i - 1); k++) {
-                    System.out.print("*");
-                }
-                System.out.println();
+            for (k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
             }
-
+            System.out.println();
         }
-    }
+        for (i = 5; i >= 1; i--) { // rows
+            for (j = 5; j > i; j--) { // space(column)
+                System.out.print(" ");
+            }
+            for (k = 1; k <= (2 * i - 1); k++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
+    }
+}
