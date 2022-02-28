6ed34aebe86881d1039b3309aeba145de1a933ca
im-kamil <kamilnasar786@gmail.com>
2022-02-28 11:11:28 +0530

    Number Pattern

---
+++ NumberPttern.java
@@ -0,0 +1,18 @@
+public class NumberPttern {
+    public static void main(String[] args) {
+        // Number pattern
+        int i, j;
+        // for (i = 1; i <= 5; i++) {
+        //     for (j = 1; j <= i; j++) {
+        //         System.out.print(j);
+        //     }
+        //     System.out.print("\n");
+        // }
+        for (i = 1; i <= 5; i++) {
+            for (j = 1; j <= i; j++) {
+                System.out.print(i);
+            }
+            System.out.print("\n");
+        }
+    }
+}
