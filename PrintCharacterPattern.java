59343bf9c1dd0a1db22667aad2ace709cb91facb
im-kamil <kamilnasar786@gmail.com>
2022-02-28 11:23:34 +0530

    Character pattern

---
+++ PrintCharacterPattern.java
@@ -0,0 +1,18 @@
+public class PrintCharacterPattern {
+    public static void main(String[] args) {
+        // print character pattern
+        char i, j;
+    //    for (i = 'A'; i <= 'E'; i++) {
+    //         for (j = 'A'; j <= i; j++) {
+    //             System.out.print(j);
+    //          }
+    //          System.out.print("\n");
+    //      } 
+         for (i = 'A'; i <= 'E'; i++) {
+            for (j = 'A'; j <= i; j++) {
+                System.out.print(i);
+             }
+             System.out.print("\n");
+         } 
+    }
+}
