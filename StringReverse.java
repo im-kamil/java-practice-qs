485667391a35f2057320f4da2c0ad498143ae6bb
im-kamil <kamilnasar786@gmail.com>
2022-03-02 13:24:03 +0530

    reverse string

---
+++ StringReverse.java
@@ -0,0 +1,10 @@
+public class StringReverse {
+  public static void main(String[] args) {
+      // string reverse 
+      StringBuffer s = new StringBuffer("Kamil kammo");
+      System.out.println(s.reverse());
+
+      StringBuilder r = new StringBuilder("faiz fa");
+      System.out.println(r.reverse());
+  }  
+}
